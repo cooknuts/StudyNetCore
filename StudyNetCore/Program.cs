88f49e197ae752e0607492b873b72a8031a6e89f
cooknuts <intel@163.com>
2022-11-24 16:53:06 +0800

    Add autofac

--- StudyNetCore/Program.cs
+++ StudyNetCore/Program.cs
@@ -1,9 +1,26 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using StudyNetCore.Api;
 using StudyNetCore.Model;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddNpgsql<postgresContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+// 以下是autofac依赖注入
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    //先注入JWT
+    //builder.RegisterType<AuthorizeJWT>().As<IAuthorizeJWT>();//可以是其他接口和类
+    // 注入Service程序集
+    Assembly assembly = Assembly.Load(ServiceAutofac.GetAssemblyName());//可以是其他程序集
+    builder.RegisterAssemblyTypes(assembly)
+    .AsImplementedInterfaces()
+    .InstancePerDependency();
+});
 
 var app = builder.Build();
 
---
+++ StudyNetCore/ServiceAutofac.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace StudyNetCore.Api
+{
+    public static class ServiceAutofac
+    {
+        /// <summary>
+        /// 获取程序集名称
+        /// </summary>
+        /// <returns></returns>
+        public static string GetAssemblyName()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Name;
+        }
+    }
+}
