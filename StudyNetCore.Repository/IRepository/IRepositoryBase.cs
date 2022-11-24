d6604d1801180cc5d8139ebe82dc6a35a4d7f7a7
intel163 <intel@163.com>
2022-11-24 10:53:46 +0800

    Fill RepositoryBase Class.

--- StudyNetCore.Repository/IRepository/IRepositoryBase.cs
+++ StudyNetCore.Repository/IRepository/IRepositoryBase.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudyNetCore.Repository.Interface
 {
-    internal interface IDALBase
+    internal interface IRepositoryBase<T> where T : class
     {
+        bool Add(T entity);
+        bool AddRange(List<T> entities);
+        T Query(Expression<Func<T, bool>> wherelambda);
+        List<T> QueryList(Expression<Func<T, bool>> wherelambda);
     }
 }
--- StudyNetCore.Repository/Repository/RepositoryBase.cs
+++ StudyNetCore.Repository/Repository/RepositoryBase.cs
@@ -1,13 +1,40 @@
-﻿using StudyNetCore.Repository.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using StudyNetCore.Model;
+using StudyNetCore.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudyNetCore.Repository.Repository
 {
-    internal class DALBase : IDALBase
+    internal class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
+        private readonly DbContext db;
+
+        public RepositoryBase(postgresContext dbEntities)
+        {
+            this.db = dbEntities;
+        }
+        public virtual bool Add(T entity)
+        {
+            db.Set<T>().Add(entity);
+            return db.SaveChanges() > 0;
+        }
+        public virtual bool AddRange(List<T> entities)
+        {
+            db.Set<T>().AddRange(entities);
+            return db.SaveChanges() > 0;
+        }
+        public virtual T Query(Expression<Func<T, bool>> whereLambda)
+        {
+            return db.Set<T>().SingleOrDefault(whereLambda);
+        }
+        public virtual List<T> QueryList(Expression<Func<T, bool>> whereLambda)
+        {
+            return db.Set<T>().Where(whereLambda).ToList();
+        }
     }
 }
